be6417d9605b730c7a500730d6f5a168f9dc976c
Miklo <kerkinni1@yahoo.com>
2017-05-15 16:41:23 -0400

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NxtRez2
+//
+//  Created by Fuat on 5/15/17.
+//  Copyright © 2017 Joseph K. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override after app launch.
+        return true
+    }
+    
+    
+}
